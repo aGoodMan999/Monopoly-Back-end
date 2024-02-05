2cacb84f9d48fe705873ef33e09bd6475a1ce2ba
aGoodMan999 <21521808@gm.uit.edu.vn>
2024-02-05 17:11:13 +0700

    Chỉnh sửa GameController: Thêm @CrossOrigin

--- src/main/java/com/annguyeen0/monopoly/controller/GameController.java
+++ src/main/java/com/annguyeen0/monopoly/controller/GameController.java
@@ -9,14 +9,11 @@ import com.annguyeen0.monopoly.respone_model.NewGameResponse;
 import com.annguyeen0.monopoly.service.GameService;
 import com.annguyeen0.monopoly.service.ParticipationService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class GameController {
 
